b66aa6d8733d96b2f198e6c8d38eae168145d4bc
blackwhite2018 <39052924+blackwhite2018@users.noreply.github.com>
2020-06-06 15:21:38 +0300

    Updates

---
+++ precache-manifest.53c0b4c281662ee6f630db6d9e9df416.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2ada6f26c464ce13d93088c0c13325",
+    "url": "/blackwhite2018/ProductCards/index.html"
+  },
+  {
+    "revision": "3874e09930e50e1c2175",
+    "url": "/blackwhite2018/ProductCards/static/css/2.2b234b19.chunk.css"
+  },
+  {
+    "revision": "30c203bb9bf773a02dac",
+    "url": "/blackwhite2018/ProductCards/static/css/main.cbfa1fbf.chunk.css"
+  },
+  {
+    "revision": "3874e09930e50e1c2175",
+    "url": "/blackwhite2018/ProductCards/static/js/2.92d86ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blackwhite2018/ProductCards/static/js/2.92d86ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c203bb9bf773a02dac",
+    "url": "/blackwhite2018/ProductCards/static/js/main.29e634fd.chunk.js"
+  },
+  {
+    "revision": "cbb815f8b511469c220a",
+    "url": "/blackwhite2018/ProductCards/static/js/runtime-main.3e45067d.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/blackwhite2018/ProductCards/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/blackwhite2018/ProductCards/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/blackwhite2018/ProductCards/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/blackwhite2018/ProductCards/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
